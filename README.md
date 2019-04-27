f36dcdf6019f64954fbfdde16516a59f9ac5f4c9
Yehor Dykhov <egor.dykhov@continuum.net>
2019-04-27 22:54:52 +0300

    NodeJs: token update on fly

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # tokenUpdate
+
+1. Need to place "tokenUpdate.js" file to root directory with "PORTAL" folder.
+2. Change LOGIN and PASSWORD in "tokenUpdate.js" file by your credentials.
+3. Login to the tunnel.
+4. Run "tokenUpdate.js" by command:
+    node tokenUpdate.js
+
+Every 3 hours your token will be updated.
---
+++ tokenUpdate.js
@@ -0,0 +1,140 @@
+const fs = require('fs');
+const https = require('https');
+
+// Set your login, password and update interval
+const LOGIN = '';
+const PASSWORD = '';
+const HOST_NAME = ''; // site.domain.net
+const PATH = ''; // /some/path?param=value&param2=value2
+const PATH_TO_FILE = ''; // ./PORTAL/index.html
+const HOURS_INTERVAL = 3; // (INT) The token will be updated every 3 hours
+
+const interval = 1000 * 60 * 60 * HOURS_INTERVAL;
+const red = '\x1b[31m';
+const green = '\x1b[32m';
+const yellow = '\x1b[33m';
+
+function getNewToken(callback) {
+    const options = {
+        method: 'POST',
+        hostname: HOST_NAME,
+        path: PATH,
+        headers: {
+            'X-OpenAM-Username': LOGIN,
+            'X-OpenAM-Password': PASSWORD,
+            'cache-control': 'no-cache',
+            'Content-Type': 'application/json',
+        }
+    };
+
+    const req = https.request(options, res => {
+        showMessage(`statusCode: ${res.statusCode}`);
+
+        const chunks = [];
+
+        res.on('data', chunk => {
+            chunks.push(chunk);
+        });
+
+        res.on('end', () => {
+            const body = Buffer.concat(chunks);
+            let result;
+
+            try {
+                result = JSON.parse(body.toString());
+                showSuccess('NEW TOKEN IS:', result.tokenId);
+            } catch (e) {
+                showError('ERROR:', e);
+                callback(null);
+            }
+
+            callback(result.tokenId);
+        });
+    });
+
+    req.on('error', error => {
+        showError('FAILED NETWORK CONNECTION:', error);
+        showError('YOU NEED TO CHECK YOUR TUNNEL OR NETWORK CONNECTION');
+    });
+
+    req.end();
+}
+
+function writeToken(token, success, failure) {
+    fs.readFile(PATH_TO_FILE, 'utf8', (err, data) => {
+        if (err) {
+            failure(err);
+            return 1;
+        }
+
+        const reg = /window\.RMM\.iPlanetDirectoryPro = ['].*['];$/;
+        const template = `window.RMM.iPlanetDirectoryPro = '${token}';`;
+        const newData = data.replace(reg, template);
+
+        fs.writeFile('./index.html', newData, 'utf8', err => {
+            if (err) {
+                failure(err);
+                return 1;
+            }
+
+            success();
+        });
+    });
+}
+
+function updateToken() {
+    getNewToken(token => {
+        if (!token) {
+            return 1;
+        }
+
+        writeToken(
+            token,
+            () => {
+                const date = new Date();
+                showSuccess('UPDATE WAS SUCCESSFUL:', date.toString());
+                showMessage(
+                    `Next update in ${HOURS_INTERVAL} ${HOURS_INTERVAL > 1 ? 'hours' : 'hour'}`
+                );
+            },
+            err => {
+                showError('Here we have some issues:', err);
+            }
+        );
+    });
+}
+
+function showError(message, error) {
+    console.log(red, `\n${message}`);
+    if (error) {
+        console.log(error);
+    }
+}
+
+function showSuccess(message, data = '') {
+    console.log(green, `\n${message} `, data);
+}
+
+function showMessage(message) {
+    console.log(yellow, `\n${message}`);
+}
+
+updateToken();
+
+function run() {
+    let timerCount = 1;
+
+    const timerId = setInterval(() => {
+        updateToken();
+        timerCount++;
+
+        console.log(timerCount);
+
+        if (timerCount === Math.round(12 / HOURS_INTERVAL)) {
+            showMessage('YOU NEED TO REST! GO HOME!');
+            clearInterval(timerId);
+        }
+    }, interval);
+}
+
+run();
