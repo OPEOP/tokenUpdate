6dc3e700e6a3c45d93a7299abb2078835bbb8890
egor_dykhov <hyoustone@yahoo.com>
2019-05-08 10:35:02 +0300

    message update

--- tokenUpdate.js
+++ tokenUpdate.js
@@ -103,10 +103,7 @@ function updateToken() {
             token,
             () => {
                 const date = new Date();
-                showSuccess('UPDATE WAS SUCCESSFUL:', date.toString());
-                showMessage(
-                    `Next update in ${HOURS_INTERVAL} ${HOURS_INTERVAL > 1 ? 'hours' : 'hour'}`
-                );
+                showSuccess('UPDATE WAS SUCCESSFUL:', date.toString());                
             },
             err => {
                 showError('Here we have some issues:', err);
@@ -139,8 +136,10 @@ function run() {
         const timerId = setInterval(() => {
             updateToken();
             timerCount++;
-
-            console.log(timerCount);
+            
+            showMessage(
+                 `Next update in ${HOURS_INTERVAL} ${HOURS_INTERVAL > 1 ? 'hours' : 'hour'}`
+            );           
 
             if (timerCount === Math.round(12 / HOURS_INTERVAL)) {
                 showMessage('YOU NEED TO REST! GO HOME!');
